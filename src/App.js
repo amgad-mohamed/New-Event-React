24e4b0b0b7bec4ce5ee5f84a94a9deee792042bd
Amgad Mohamed <amgagmohamed38@gmail.com>
2022-10-29 20:39:05 +0200

    ourProgram response comp

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 // import Overveiw from "./design-system/componants/Overveiw/Overveiw";
 // import overveiwData from "./design-system/componants/Overveiw/Data";
+// import { faUserGroup, faClockFour, faBriefcaseClock } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+
 import speakersData from "./design-system/componants/CreativeSpeakers/SpeakersData.jsx";
 import CreativeSpeakers from "./design-system/componants/CreativeSpeakers/CreativeSpeakers.jsx";
-// import { faUserGroup, faClockFour, faBriefcaseClock } from "@fortawesome/free-solid-svg-icons";
+import "../src/design-system/componants/main.css";
+import OurPrograms from "./design-system/componants/OurPrograms/OurPrograms.jsx";
+import { OurProgramsData } from "./design-system/componants/OurPrograms/OurProgramsData.jsx";
 
 function App() {
   return (
-    <div>
+    <>
       <div style={{ display: "flex" }}>
-      {speakersData.map(item =>{
-        return(
-          <CreativeSpeakers 
-          img={item.img}
-          alt={item.alt}
+        {speakersData.map((item) => {
+          return (
+            <CreativeSpeakers
+              img={item.img}
+              alt={item.alt}
+              name={item.name}
+              job={item.job}
+              key={item.id}
+            />
+          );
+        })}
+      </div>
+      <div>{OurProgramsData.map((item)=>{
+        return ( 
+           <OurPrograms 
+           key={item.id}
+           img={item.img}
+           alt={item.alt}
           name={item.name}
-          job={item.job}
-          key={item.id}
-          />
-        )
+          title={item.title}
+          description={item.description}
+          time={item.time}
+          roomNumber={item.roomNumber}
+           />)
       })}
       </div>
-    </div>
+    </>
   );
 }
 
---
+++ src/design-system/componants/OurPrograms/OurProgramsData.jsx
@@ -0,0 +1,36 @@
+import logo from "../../../shared/ourProgram/firstday-1.jpg";
+import logo1 from "../../../shared/ourProgram/firstday-2.jpg";
+import logo2 from "../../../shared/ourProgram/firstday-3.jpg";
+
+export const OurProgramsData = [
+  {
+    id: 1,
+    img: logo,
+    name:"",
+    alt: "image1",
+    title: "Introduction TO Design",
+    description: "faDownLeftAndUpRightToCenterfaDownLeftAndUpRightToCenter",
+    time: " 09.00 AM",
+    roomNumber: " 240",
+  },
+  {
+    id: 2,
+    img: logo1,
+    name:"By Johnathan Mark",
+    alt: "image2",
+    title: "Front-End Development",
+    description: "faDownLeftAndUpRightToCenterfaDownLeftAndUpRightToCenter",
+    time: " 01.00 AM",
+    roomNumber: " 360",
+  },
+  {
+    id: 3,
+    img: logo2,
+    name:"By Hazem Mohamed",
+    alt: "image3",
+    title: "Social Media Marketing",
+    description: "faDownLeftAndUpRightToCenterfaDownLeftAndUpRightToCenter",
+    time: " 11.00 AM",
+    roomNumber: " 450",
+  },
+];
